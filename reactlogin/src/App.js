fe152d849b96d47168cf3d2df0317da9198d62a9
jeonjuseong11 <111875481+jeonjuseong11@users.noreply.github.com>
2023-04-27 11:14:38 +0900

    Feat : useForm
    useForm 사용해보기

--- reactlogin/src/App.js
+++ reactlogin/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"; //
+import * as yup from "yup";
 
-function App() {
+export default function App() {
+  const formSchema = yup.object({
+    email: yup
+      .string()
+      .required("이메일을 입력해주세요")
+      .email("이메일 형식이 아닙니다."),
+    password: yup
+      .string()
+      .required("영문, 숫자포함 8자리를 입력해주세요.")
+      .min(8, "최소 8자 이상 가능합니다")
+      .max(15, "최대 15자 까지만 가능합니다")
+      .matches(
+        /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,15}$/,
+        "영문 숫자포함 8자리를 입력해주세요."
+      ),
+    passwordConfirm: yup
+      .string()
+      .oneOf([yup.ref("password")], "비밀번호가 다릅니다."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Input Test!</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="email" placeholder="이메일" {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          type="password"
+          name="password"
+          placeholder="비밀번호"
+          {...register("password")}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <input
+          type="password"
+          name="passwordConfirm"
+          placeholder="비밀번호 확인"
+          {...register("passwordConfirm")}
+        />
+        {errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+        <input type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
